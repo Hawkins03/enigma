e1bd4b073c822796917418f015532565b50c9a49
Hawkins03 <falcor.peterson@gmail.com>
2021-09-30 21:35:52 -0400

    made the backend work (I still need to test)
    made initalize settings method that initalizes rotors, pairs, rotor_settings, and alpha vars
    
    also made rotor() that opperates a single rotor and doesn't change any rotor settings while doing so (need to make that happen)
    made plugboard() that runs a char through the plugboard
    made encrypt function that first runs the keypress through the plugboard, the rotors, the reflector, back through the rotors and through the plugboard again.
    
    todo:
    make rotor_list pull from a file so that all lines are under 120 words whenever possible
    update rotor settings

--- backend.py
+++ backend.py
@@ -5,27 +5,17 @@ from enum import Enum
 a = open('encoded_txt.txt','w')
 a.close()
 
-class Direction(Enum): # list is [x_direction,y_direction]
-    LEFT  = [-1,0]
-    RIGHT = [1, 0]
-    UP    = [0,-1]
-    DOWN  = [0, 1]
-
 '''
 
 changing settings:
 
 '''
-class enigma():
+class backend():
     def __init__(self): 
-        self.generate_settings()
+        self.generate_setting_list()
+        self.inatalize_settings()        
 
-        #initalizing rotors
-        self.rotor_1_settings = self.settings_list[0][0]
-        self.rotor_2_settings = self.settings_list[0][1]
-        self.rotor_3_settings = self.settings_list[0][2]
-    
-    def generate_settings(self):
+    def generate_setting_list(self):
         '''
         Line 1: rotor settings
         Line 2: rotor selections
@@ -48,142 +38,51 @@ class enigma():
                     breaks.append(j)
         self.settings_list = formatted_settings_list 
         print(settings_list)
+
+    def initalize_settings(self):
+        #initalizing rotors
+        self.rotor_settings = []
+        for i in range(3):
+            self.rotor_settings.append(self.settings_list[0][i])
+
+        #generating rotors
+        rotor_list=['ekmflgdqvzntowyhxuspaibrcj','ajdksiruxblhwtmcqgznpyfvoe','bdfhjlcprtxvznyeiwgakmusqo','esovpzjayquirhxlnftgkdcmwb','vzbrgityupsdnhlxawmjqofeck','jpgvoumfyqbenhzrdkasxlictw','nzjhgrcxmyswboufaivlpekqdt','fkqhtlxocbjspdzramewniuygv']
+        self.rotors = []
+        for i in settings_list[1]:
+            self.rotors.append(rotor_list[i]) # reading in the selected rotors
+        self.rotors.append('ejmzalyxvbwfcrquontspikhgd')
+
+        #other rotors settings
+        self.alpha='abcdefghijklmnopqrstuvwxyz'
+
+        #plugboard pairs
+        self.pairs = [settings_list[2],settings_list[3]]    
     
-    def end_screen(self, screen):
-        screen.clear()
-        curses.endwin()
+    def rotor(self, rotor_number, plaintext):
+        for i in range(0,len(plaintext)):
+            if plaintext == self.alpha[i]:
+                return rotor[rotor_number][i-rotor_settings[rotor_number]
 
+    def plugboard(self,plaintext):
+        for i in range(10):
+            if plaintext == self.pairs[0][i]:
+                plaintext = self.pairs[1][i]
+            if plaintext == self.pairs[1][i]:
+                plaintext = self.pairs[0][i]
+        return plaintext
 
+    def encrypt(plaintext):
+        plaintext = plugboard(plaintext)
+        for i in range(4):
+            rotor(i,plaintext)
+        for i in range(2,-1):
+            rotor(i,plaintext)
+        plaintext = plugboard(plaintext)
+        print(plaintext)
+        return plaintext
 
-    def draw_line(x_start, y_start, direction):
-        if not isinstance(direction, Direction):
-            raise TypeError("direction must be an instance of Direction Enum")
-
-
-        
-
-
-enigma()
 
 '''
-def settings(input_txt):
-    dims = 
-    print(input_txt)
-    breaks = [0]
-    for i in range(0,len(txt)):
-        if txt[i] == ' ':
-            breaks.append(i) #looking for spaces and appending them to the breaks list
-    try:
-        nrp.append(int(txt[0:breaks[1]])) # appending the first two captured in the breaks list to nrp
-    except:
-        print(breaks)
-        nrp.append(txt[0:breaks[1]])    
-    for i in range(1,len(breaks)-1):
-        try:
-            nrp.append(int(txt[breaks[i]+1:breaks[i+1]]))
-        except:
-            nrp.append(txt[breaks[i]+1:breaks[i+1]])      
-    c = breaks[len(breaks)-1]
-    try:
-        nrp.append(int(txt[c+1:c+2]))
-    except:
-        nrp.append(txt[c+1:c+2])
-    return(nrp)
-for i in range(0,4):
-    new_nrp.append(settings(nrp[i]))
-nrp = new_nrp
-
-
-Done Changing Settings
-
-
-screen = curses.initscr()
-screen.keypad(1)
-curses.echo(0)
-curses.curs_set(0)
-screen.clear()
-screen.refresh()
-dims = screen.getmaxyx()
-
-r_1_s = nrp[0][0]
-r_2_s = nrp[0][1]
-r_3_s = nrp[0][2]
-
-def draw_rotors(y,x,nrp,n):
-  screen.addstr(y,x,'__')
-  for i in range(0,3):
-    screen.addch(y+2+2*i,x,'-')
-    screen.addch(y+2+2*i,x+1,'-')
-  for i in range(0,5):
-    screen.addch(y+1+i,x-1,'|')
-    screen.addch(y+1+i,x+2,'|')
-  for i in range(0,3):
-    if len(str(nrp[0][n]-1+i))==1:
-      screen.addch(y+1+2*i,x,'0')
-      screen.addch(y+1+2*i,x+1,str((nrp[0][n]-1+i)%26))
-    else:
-      screen.addstr(y+1+2*i,x,str((nrp[0][n]-1+i)%26))
-def draw_plugs(y,x,nrp):
-  screen.addch(y-1,x+1,'_')
-  screen.addch(y-1,x+2,'_')
-  screen.addch(y-1,x+3,'_')
-  for i in range(1,11):
-    screen.addch(y+2*i-1,x+3,'-')
-    screen.addch(y+2*i-1,x+2,'-')
-    screen.addch(y+2*i-1,x+1,'-')
-  for i in range(0,19):
-    screen.addch(y+i,x,'|')
-    screen.addch(y+i,x+4,'|')
-    screen.addch(y+i,x+2,'|')
-
-  for i in range(0,10):
-    screen.addch(y+2*i,x+1,nrp[2][i])
-    screen.addch(y+2*i,x+3,nrp[3][i])
-
-graphics = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-def draw_scr():
-  y_half = int(dims[0]/2-2)
-  y_three_quarters = int(y__2+y__2/2-5)
-  y_one_quarter = int(y__2-y__2/2)
-  x_half = int((dims[1]-1)/2)
-  x_one_quarter = int(x__2/2)
-  x_three_quarters = x__2+int(x_1_4/2)
-  banner = "THE WWII ENIGMA MACHINE"
-  screen.addstr(1,int(x__2-(len(banner)/2),banner))
-  for i in range(0,dims[1]):
-    screen.addch(5,i,'_')
-  for i in range(6,dims[0]):
-    screen.addch(i,x_1_4,'|')
-  for i in range(x_1_4+1,dims[1]):
-    screen.addch(y_3_4,i,'-')
-
-
-def draw_keyboard():
-    keyboard_layout="qwertyuiopasdfghjklzxcvbnm"
-    #switcher={10:(5,-41),18:(11,-38),26:(17,-35)} #index of last value:(y offset, first x offset)
-    for i in range(0,26):
-        #updating the chordinates of the letter (y_chord,starting x chord, smallest i value in the group
-        if i<=10:
-            switcher = (5,-41,0)
-        elif i<=18:
-            switcher = (11,-38,11)
-        else:
-            switcher = (17,-35,18)
-        curr_letter = keyboard_layout[i]
-        screen.addch(y_3_4+switcher[0],x_3_4+switcher[1]+i*9,curr_letter,graphics[1][ord(curr_letter)-97])
-
-
-  x__2 = x_3_4-2
-  y__2 = y_1_4-1
-  screen.addstr(y_1_4+6,x__2-12,'0'+str(nrp[1][2]))
-  screen.addstr(y_1_4+6,x__2,'0'+str(nrp[1][1]))
-  screen.addstr(y_1_4+6,x__2+12,'0'+str(nrp[1][0]))
-
-  draw_rotors(y__2,x__2,nrp,1)
-  draw_rotors(y__2,x__2-12,nrp,2)
-  draw_rotors(y__2,x__2+12,nrp,0)
-
-  draw_plugs(y__2-2,x__2-36,nrp)
 def plugboard(action,pairs):
   for i in range(0,10):
     if action == pairs[0][i]:
