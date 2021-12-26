59f823467e3dc5ee0d853dae57a97faf2573857b
Hawkins03 <falcor.peterson@gmail.com>
2021-12-26 14:50:06 -0500

    added zoom out functionality and moved to a class based structure because there were too many params (also started folowing coding style)

--- main.py
+++ main.py
@@ -1,188 +1,192 @@
 import curses
-import sys
 import textwrap
-import copy
-from enum import Enum
 import encoding
 
+
 '''
-screen for the enigma project. if you have trouble with the screen just use encoding.py from the terminal
+screen for the enigma project.
+if you have trouble with the screen just use enoding.py from the terminal
 
 @author Hawkins Peterson
 @version 12.19.21
 '''
 
-'''
-Returns the y midpoint of the selected screen (note, rounds down)
-@return the y midpoint of the selected screen (note, rounds down)
-'''
-def getYMid(screen):
-    return int(screen.getmaxyx()[0] / 2)
 
-'''
-Returns the x midpoint of the selected screen (note, rounds down)
-@return the x midpoint of the selected screen (note, rounds down)
-'''
-def getXMid(screen):
-    return int(screen.getmaxyx()[1] / 2)
-
-'''
-Returns the last 12 lines of a string wrapped at length "width"
-@return the last 12 lines of a string wrapped at length "width"
-@param text the text to format
-@param width the width of the text to wrap it at
-'''
-def formatString(text, width):
-    return ''.join(textwrap.wrap(text, width = width)[-5:])
-
-'''
-ends the screen
-@param screen the screen needed to end
-'''
-def endScreen(screen):
-    screen.clear()
-    curses.endwin()
-
-
-'''
-constructs a blank screen with only borders and the title
-@param header the header subScreen
-@param screen the main screen
-'''
-def drawBlankScreen(header, screen):
-    screen.box()
-    header.hline(8, 0, "-", screen.getmaxyx()[1])
-    header.addstr(3, getXMid(header) - 9, "THE ENIGMA MACHINE")
-    screen.refresh()
-
-
-
-def drawSettings(screen, rotors, output, keyboard):
-    screen.refresh()
-
-    rotors.box()
-    rotors.addstr(0, getXMid(rotors) - 7, "ROTOR SETTINGS:")
-    rotors.refresh()
-
-    output.box()
-    output.addstr(0, getXMid(output) - 11, "PLAINTEXT / CIPHERTEXT")
-    output.refresh()
-    
-    keyboard.box()
-    keyboard.addstr(0, getXMid(keyboard) - 4, "KEYBOARD")
-    keyboard.refresh()
-
-def drawOutput(output, plaintext, ciphertext):
-    yMax = output.getmaxyx()[0]
-    xMax = output.getmaxyx()[1]
-    output.addstr(0, 0, "Plaintext:")
-    output.addstr(5, 0, "Ciphertext:")
-    output.addstr(1, 0, formatString(plaintext, xMax))
-    output.addstr(6, 0, formatString(ciphertext, xMax))
-    output.refresh()
-
-def drawRotors(rotors, rotorArray):
-    for y in range(3):
-        x = getXMid(rotors) + y * 5 - 5
-        rotors.vline(4, x - 1, "|", 5)
-        rotors.vline(4, x + 1, "|", 5)
-        rotors.addch(2, x, str(rotorArray[0][y]))
-        for z in range(3):
-            rotors.addstr(4 + 2 * z, x, rotorArray[1][y][z])
-    rotors.refresh()
-
-'''
-draws the keyboard
-@param screen the screen used by the keyboard
-@param keyPressed a int telling which key is currently pressed down
-'''
-def drawKeyboard(keyboard, highlightedKey = " "):
-    keyAlphabet = "qwertyuiopasdfghjklzxcvbnm"
-    xPosBases = [41, 45, 36]
-    try:
-        for i in range(26):
-            row = int((i - 1) / 9)
-            yPos = row * 6 + 1 #0-9 = 2, 10-18 = 8, 19-25 = 14
-            xPos = getXMid(keyboard) - xPosBases[row] + (i - row * 9) * 9
-            if highlightedKey == keyAlphabet[i]:
-                keyboard.addch(yPos, xPos, ord(keyAlphabet[i]), curses.A_REVERSE)
-            else:
-                keyboard.addch(yPos, xPos, ord(keyAlphabet[i]))
-        keyboard.refresh();
-    except Exception as e:
-        dims = keyboard.getmaxyx()
+class enigma:
+    def __init__(self):
+        self.screen = curses.initscr()      # starting the screen
+        self.screen.keypad(True)            # accepts ctrl and other weird keys
+        curses.echo(False)                  # doesn't show which key pressed
+        curses.curs_set(False)              # doesn't show cursor
+        self.screen.clear()                 # clears the screen
+        self.screen.refresh()               # refresh
+        dims = self.screen.getmaxyx()            # get screen dims
         yMax = dims[0]
         xMax = dims[1]
-        endScreen(screen)
-        print(e)
-        print("yMax: " + str(yMax) + " xMax: " + str(xMax))
-        print("yPos: " + str(yPos) + " xPos: " + str(xPos))
 
-#try block because errors are a GIANT pain in curses
-try:
-    #init screen (blank canvas)
-    screen = curses.initscr() #starting the screen
-    screen.keypad(True) #accepts ctrl and other weird keys
-    curses.echo(False) #doesn't show which key pressed
-    curses.curs_set(False) #doesn't show cursor
-    screen.clear() #clears the screen
-    screen.refresh() #refresh
-    dims = screen.getmaxyx() #get screen dims
-    yMax = dims[0]
-    xMax = dims[1]
+        # constructing all the subscreens
+        self.header = self.screen.subwin(9, xMax - 2, 0, 1)
+        activeWin = self.screen.subwin(yMax - 8, xMax, 8, 0)
 
-    #constructing all the subscreens
-    header = screen.subwin(9, xMax - 2, 0, 1)
-    activeWin = screen.subwin(yMax - 8, xMax, 8, 0)
+        self.keyboard = activeWin.subwin(15, 100, yMax - 16, 43)
+        self.output = activeWin.subwin(10, xMax - 7, 21, 4)
+        self.outLine = activeWin.subwin(12, xMax - 5, 20, 3)
+        self.rotors = activeWin.subwin(11, 30, 9, self.getXMid(activeWin) - 15)
 
-    keyboard = activeWin.subwin(15, 100, yMax - 16, int((xMax - 100) / 2))
-    output = activeWin.subwin(10, xMax - 7, 21, 4)
-    outLine = activeWin.subwin(12, xMax - 5, 20, 3)
-    rotors = activeWin.subwin(11, 30, 9, getXMid(activeWin) - 15)
-    
-    encrypter = encoding.Encoding()
-    rotorSettings = encrypter.getFancyRotorSettings()
-    plaintext =  ""
-    ciphertext = ""
-    keyboardAlphabet = "qwertyuiopasdfghjklzxcvbnm"
+        self.encrypter = encoding.Encoding()
+        self.rotorSettings = self.encrypter.getFancyRotorSettings()
+        self.plaintext = ""
+        self.ciphertext = ""
+        self.keyboardAlphabet = "qwertyuiopasdfghjklzxcvbnm"
 
-    #drawing basic stuff
-    drawBlankScreen(header, screen)
-    #drawSettings(screen, rotors, outLine, keyboard)
-    drawKeyboard(keyboard)
-    drawRotors(rotors, encrypter.getFancyRotorArray())
-    drawOutput(output, plaintext, ciphertext)
-    
+    def getYMid(self, screen):
+        return int(screen.getmaxyx()[0] / 2)
 
-    #loop
-    action = 0
-    allowed = [".", ",", " ", "!", "?"]
-    while (True):
-        #getting keypress
-        action = ord(chr(screen.getch()).lower()) #ord and chr to convert uppercase letters to lowercase letters
-        if action == 27:
-            break
-        elif action >= 97 and action <= 122: #normal a-z lowercase letters
-            plaintext += chr(action)
-            ciphertext += encrypter.encrypt(chr(action), False)
-        for a in allowed: #checking for punctuation and just adding it
-            if action == ord(a):
-                plaintext += a
-                ciphertext += a
-        drawKeyboard(keyboard, ciphertext[-1])#last encrypted thingey
-        drawRotors(rotors, encrypter.getFancyRotorArray())
-        drawOutput(output, plaintext, ciphertext)
-        screen.refresh()
-    
-    endScreen(screen)
-    encrypter.close(False)
-    outputTxt = "%s \n\nplaintext:  %s \nciphertext: %s\n\n" % (rotorSettings, plaintext, ciphertext)
-    print(outputTxt)
-    print(action)
-    f = open("output.txt", "a");
-    f.write(outputTxt)
-    f.close()
-except Exception as e:
-    endScreen(screen)
-    print(e)
+    def getXMid(self, screen):
+        return int(screen.getmaxyx()[1] / 2)
 
+    def formatString(self, text, width):
+        return ''.join(textwrap.wrap(text, width)[-5:])
+
+    def endScreen(self):
+        self.screen.clear()
+        curses.endwin()
+
+    def drawBlankScreen(self):
+        mid = self.getXMid(self.header)
+        self.screen.box()
+        self.header.hline(8, 0, "-", self.screen.getmaxyx()[1])
+        self.header.addstr(3, mid - 9, "THE ENIGMA MACHINE")
+        self.screen.refresh()
+
+    def drawSettings(self, selected):
+        self.screen.refresh()
+        rMid = self.getXMid(self.rotors)
+        kMid = self.getXMid(self.keyboard)
+        self.keyboard.box()
+        if selected % 2 == 0:
+            rotors.box()
+            mid = self.getXMid(rotors)
+            rotors.addstr(0, mid-7, "ROTOR SETTINGS:", curses.A_REVERSE)
+            rotors.refresh()
+        else:
+            self.keyboard.addstr(0, mid - 4, "KEYBOARD", curses.A_REVERSE)
+            self.keyboard.refresh()
+            rotors.box()
+            rotors.addstr(0, mid - 7, "ROTOR SETTINGS:")
+            rotors.refresh()
+
+        mid = self.getXMid(self.output)
+        self.output.box()
+        self.output.addstr(0, mid-11, "PLAINTEXT / CIPHERTEXT")
+        self.output.refresh()
+
+        mid = self.getXMid(self.keyboard)
+        if selected % 3 == 2:
+                    else:
+            self.keyboard.box()
+            self.keyboard.addstr(0, mid - 4, "KEYBOARD")
+            self.keyboard.refresh()
+
+    def drawOutput(self):
+        xMax = self.output.getmaxyx()[1]
+
+        self.output.addstr(0, 0, "Plaintext:")
+        self.output.addstr(5, 0, "Ciphertext:")
+        self.output.addstr(1, 0, self.formatString(self.plaintext, xMax))
+        self.output.addstr(6, 0, self.formatString(self.ciphertext, xMax))
+        self.output.refresh()
+
+    def drawRotors(self, rotorArray):
+        for y in range(3):
+            x = self.getXMid(self.rotors) + y * 5 - 5
+            self.rotors.vline(4, x - 1, "|", 5)
+            self.rotors.vline(4, x + 1, "|", 5)
+            self.rotors.addch(2, x, str(rotorArray[0][y]))
+            for z in range(3):
+                self.rotors.addstr(4 + 2 * z, x, rotorArray[1][y][z])
+        self.rotors.refresh()
+
+    def drawKeyboard(self, highlightedKey=" "):
+        keyAlphabet = "qwertyuiopasdfghjklzxcvbnm"
+        xPosBases = [41, 45, 36]
+        try:
+            for i in range(26):
+                row = int((i - 1) / 9)
+                yPos = row * 6 + 1  # 0-9 = 2, 10-18 = 8, 19-25 = 14
+                mid = self.getXMid(self.keyboard)
+                xPos = mid - xPosBases[row] + (i - row * 9) * 9
+                if highlightedKey == keyAlphabet[i]:
+                    o = ord(keyAlphabet[i])
+                    self.keyboard.addch(yPos, xPos, o, curses.A_REVERSE)
+                else:
+                    self.keyboard.addch(yPos, xPos, ord(keyAlphabet[i]))
+            self.keyboard.refresh()
+        except Exception as e:
+            self.endScreen()
+            print(e)
+            print("yMax: " + str(self.yMax) + " xMax: " + str(self.xMax))
+            print("yPos: " + str(yPos) + " xPos: " + str(xPos))
+
+    def machineLoop(self):
+        # loop
+        action = 0
+        allowed = [".", ",", " ", "!", "?"]
+        while (True):
+            # getting keypress
+            action = ord(chr(self.screen.getch()).lower())
+            if action == 27:
+                return 0
+            elif action >= 97 and action <= 122:
+                self.plaintext += chr(action)
+                self.ciphertext += self.encrypter.encrypt(chr(action), False)
+            for a in allowed:
+                if action == ord(a):
+                    self.plaintext += a
+                    self.ciphertext += a
+            self.drawKeyboard(self.ciphertext[-1])
+            self.drawRotors(self.encrypter.getFancyRotorArray())
+            self.drawOutput()
+            self.screen.refresh()
+
+    def settingsLoop(self):
+        action = 0
+        selected = 2
+        while (True):
+            self.drawSettings(selected)
+            action = self.screen.getch()
+            if action == 27:
+                return -1
+            elif action == curses.KEY_UP:
+                selected -= 1
+            elif action == curses.KEY_DOWN:
+                selected += 1
+            elif action == ord("\n"):
+                return selected
+
+    def run(self):
+        self.drawBlankScreen()
+        self.drawKeyboard()
+        self.drawRotors(self.encrypter.getFancyRotorArray())
+        self.drawOutput()
+
+        try:
+            while True:
+                self.machineLoop()
+                self.settingsLoop()
+            self.endScreen(self.screen)
+            self.encrypter.close(False)
+            i = (self.rotorSettings, self.plaintext, self.ciphertext)
+            outputTxt = "%s \n\nplaintext:  %s \nciphertext: %s\n\n" % i
+            print(outputTxt)
+            f = open("output.txt", "a")
+            f.write(outputTxt)
+            f.close()
+
+        except Exception as e:
+            self.endScreen()
+            print(e)
+
+
+enigma = enigma()
+enigma.run()
